2fde7f8922d2102446517232ffa1bc3ab5c4cf3b
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2026-09-04 19:06:11 +0300

    fix subdcription entity

--- src/Laraue.Apps.Billing.DataAccess/Entities/Subscription.cs
+++ src/Laraue.Apps.Billing.DataAccess/Entities/Subscription.cs
@@ -4,10 +4,10 @@ public class Subscription
 {
     public required Guid Id { get; set; }
     
-    public Guid ServiceId { get; set; }
+    public ServiceId ServiceId { get; set; }
     public Service? Service { get; set; }
     
-    public Guid TariffId { get; set; }
+    public TariffId TariffId { get; set; }
     public Tariff? Tariff { get; set; }
     
     /// <summary>
